8153670442c55f960523885ca23cdd9b4eefbaf7
sebaszuniga12 <sebaszuni12@yahoo.com>
2021-03-22 22:50:00 -0500

    API

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-	space: process.env.REACT_APP_API_SPACE,
-	accessToken: process.env.REACT_APP_ACCESS_TOKEN
+	space: "zgvmht6ci6qu",
+	accessToken: "lVFsiirvkKOn6p1BHeR4PBaytTix-OJlGbnmGh-gR2M"
 });
